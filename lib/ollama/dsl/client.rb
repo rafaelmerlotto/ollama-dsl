e40f453242cbfeeb645b1e806777b1b5c8d82f75
Rafael <rafaelmerlotto@gmail.com>
2025-08-22 05:59:16 +0200

    Add initial stream method

--- lib/ollama/dsl/client.rb
+++ lib/ollama/dsl/client.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
+require "net/http"
+require "uri"
+
 
 module Ollama
   module Dsl
     class Client
       def initialize(host: "http://localhost:11434")
+        @ollama_host = URI(host)
+      end
 
+      def stream(path)
+        uri = "#{@ollama_host} #{path}".strip
+        req = Net::HTTP::Post.new(uri, { "Content-Type" => "application/json" })
       end
     end
   end
