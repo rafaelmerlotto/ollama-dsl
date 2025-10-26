32571862ea33231de76294a50e9a13eea94eab8b
Rafael <rafaelmerlotto@gmail.com>
2025-10-26 21:24:59 +0100

    Include json to execute stream

--- lib/ollama/dsl/client.rb
+++ lib/ollama/dsl/client.rb
@@ -2,7 +2,7 @@
 
 require "net/http"
 require "uri"
-
+require "json"
 
 module Ollama
   module Dsl
@@ -11,9 +11,26 @@ module Ollama
         @ollama_host = URI(host)
       end
 
-      def stream(path)
-        uri = "#{@ollama_host} #{path}".strip
+      def stream(path, payload)
+
+        uri = URI("#{@ollama_host}#{path}")
+
+        # POST request
         req = Net::HTTP::Post.new(uri, { "Content-Type" => "application/json" })
+        req.body = payload.to_json
+
+        # Response streaming
+        Net::HTTP.start(uri.hostname, uri.port) do |http|
+          http.request(req) do |response|
+            response.read_body do |chunk|
+              begin
+                json = JSON.parse(chunk)
+                yield json if block_given?
+              rescue JSON::ParserError
+              end
+            end
+          end
+        end
       end
     end
   end
--- spec/ollama/dsl_spec.rb
+++ spec/ollama/dsl_spec.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
+require "rspec"
+require_relative "../../lib/ollama/dsl/dsl"
 
 RSpec.describe Ollama::Dsl do
   it "has a version number" do
     expect(Ollama::Dsl::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
